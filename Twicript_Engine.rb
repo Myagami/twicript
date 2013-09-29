c0c6e8a071977a799ee6ac4c9a51831d99382756
M.Yagami <hayate@Reinforce-Eins.(none)>
2013-09-29 21:08:45 +0900

    header errer check

--- Twicript_Engine.rb
+++ Twicript_Engine.rb
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 class Twicript_Engine
-  def start()
+  def initialize()
     i=0
     while i < 30
       print "-"
@@ -12,6 +13,25 @@ class Twicript_Engine
       print "-"
       i += 1
     end
-    print "\n"
+    print "\n\n"
+  end
+
+  def loading_code(file)
+    begin #処理
+      src = File.readlines(file) 
+      #ヘッダ判定
+      if /^#twicript$/ =~ src[0] then
+        puts "true"
+      else
+        raise "Error Header Code"
+      end
+      for i in 1 .. src.size
+        puts src[i]
+      end
+
+    rescue => err # 例外処理
+      puts err
+    end
+
   end
 end
