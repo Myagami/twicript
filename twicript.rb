6a7cb6bda08a570b979c022d74d99444592b6e37
M.Yagami <hayate@Reinforce-Eins.(none)>
2013-09-29 20:48:09 +0900

    use class

--- twicript.rb
+++ twicript.rb
@@ -5,10 +5,19 @@
 require File.dirname(__FILE__) + "/Twicript_Engine"
 
 #インスタンス生成 / 起動画面出力
-#engine = Twicript_Engine.new
+engine = Twicript_Engine.new
 
 #スクリプト読み込み
-tw_src = ARGV[0]
-print "\n"
-puts tw_src
+tw_src_name = ARGV[0]
+
+#コード読出
+begin
+  if /^[a-zA-Z0-9_]{1,}\.twc$/ =~ tw_src_name then
+    engine.loading_code(tw_src_name)
+  else
+    raise "file add type error!"
+  end
+rescue => err
+  puts err
+end
 
