d88c21e5e7218e02d05124a41f4816af5486a7f5
M.Yagami <hayate@Reinforce-Eins.(none)>
2013-09-29 12:08:04 +0900

    twicript start

---
+++ Twicript_Engine.rb
@@ -0,0 +1,17 @@
+class Twicript_Engine
+  def start()
+    i=0
+    while i < 30
+      print "-"
+      i += 1
+    end
+    print "\n"
+    print "Twicript for Command Line\n"
+    i=0
+    while i < 30
+      print "-"
+      i += 1
+    end
+    print "\n"
+  end
+end
---
+++ twicript.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+#基礎エンジン呼び出し
+require File.dirname(__FILE__) + "/Twicript_Engine"
+
+#インスタンス生成 / 起動画面出力
+engine = Twicript_Engine.new
+engine.start()
+
+#スクリプト読み込み
+
+
