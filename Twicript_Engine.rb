fd738f8bc6d4741b123cbab89584be8243c0929b
M.Yagami <hayate@Reinforce-Eins.(none)>
2013-09-30 13:42:49 +0900

    filter

--- Twicript_Engine.rb
+++ Twicript_Engine.rb
@@ -20,17 +20,25 @@ class Twicript_Engine
     begin #処理
       src = File.readlines(file) 
       #ヘッダ判定
-      if !/^#twicript$/ =~ src[0] then
+      if /^twicript/ =~ src[0] then
         raise "Error Header Code"
       end
 
       for i in 1 .. src.size
-        puts src[i]
+        syntax_check(src[i])
       end
 
     rescue => err # 例外処理
       puts err
     end
+  end
 
+  def syntax_check(line) # 構文チェック
+    if /[A-Z]{1,}_[a-z]{1};/ =~ line # 出力系処理
+      puts line 
+    elsif /[A-Z]{1,}_[a-z]{1},[a-z]{1}=[a-z]{1,};/ =~ line # 演算系処理
+      puts "Math:"+line 
+    end
+    
   end
 end
