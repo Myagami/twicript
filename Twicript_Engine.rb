b8220c9642fcd23d955bd3b781d9b3e31805d3fa
M.Yagami <hayate@Reinforce-Eins.(none)>
2013-10-23 22:48:44 +0900

    add load module

--- Twicript_Engine.rb
+++ Twicript_Engine.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 require File.dirname(__FILE__) + "/Core/Twicript_Core"
+require File.dirname(__FILE__) + "/Core/Twicript_Math"
+
 
 class Twicript_Engine
   include Twicript_Core # => 関数系
